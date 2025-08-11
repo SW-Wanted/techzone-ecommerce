e7d5df9ce337011afb62e022a7751237d4c4695f
Emanuel dos Santos <ernelemanuel10@hotmail.com>
2025-08-11 19:42:41 +0100

    feat: encritação da pasword com bcryptjs adicionado (closes #2)

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
 	{
@@ -28,6 +29,22 @@ const userSchema = new mongoose.Schema(
 		timestamps: true,
 	}
 );
+
+// 2. Adicionar o middleware "pre-save"
+// Esta função será executada automaticamente ANTES de um documento ser guardado.
+
+userSchema.pre('save', async function (next) {
+  // Apenas encripta a password se ela foi modificada (ou é nova)
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  // Gera um "salt" - uma string aleatória para tornar o hash mais seguro
+  const salt = await bcrypt.genSalt(10);
+  // Substitui a password em texto simples pela password encriptada (hashed)
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
